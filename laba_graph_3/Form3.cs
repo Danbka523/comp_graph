433b0358f22ba3421e33ef77445cfedbed867172
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-09-30 13:25:12 +0300

    task 2 fix by EIvanov

--- laba_graph_3/Form3.cs
+++ laba_graph_3/Form3.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,42 +18,61 @@ namespace laba_graph_3
 
         Graphics g;
         Bitmap bmp;
+        private Color foreColor;
         public Form3()
         {
             InitializeComponent();
-            g=CreateGraphics();
+            g = CreateGraphics();
             bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
-       
-     
+
+
 
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
+            CheckMethod();
             if (isfirst)
             {
                 isfirst = false;
                 x1 = e.Location.X;
                 y1 = e.Location.Y;
+                //pictureBox1.Invalidate();
             }
             else
             {
                 isfirst = true;
                 x2 = e.Location.X;
                 y2 = e.Location.Y;
-                CheckMethod();
-                pictureBox1.Invalidate();   
-                
-              
+                pictureBox1.Invalidate();
+                // CheckMethod();
+
+
             }
         }
-
         protected override void OnPaint(PaintEventArgs e)
         {
 
-           pictureBox1.Invalidate();
-           pictureBox1.Image = bmp;
+            pictureBox1.Invalidate();
+            pictureBox1.Image = bmp;
         }
+
+        int ipart(double x) { return (int)x; }
+
+        int round(double x) { return ipart(x + 0.5); }
+
+        double fpart(double x)
+        {
+            if (x < 0) return (1 - (x - Math.Floor(x)));
+            return (x - Math.Floor(x));
+        }
+
+        double rfpart(double x)
+        {
+            return 1 - fpart(x);
+        }
+
+
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
@@ -66,7 +84,7 @@ namespace laba_graph_3
                 int sx = (x1 < x2) ? 1 : -1;
                 int sy = (y1 < y2) ? 1 : -1;
                 int err = dx - dy;
-                Pen p = new Pen(Color.Red,1);
+                Pen p = new Pen(Color.Red, 1);
 
                 while (true)
                 {
@@ -95,61 +113,98 @@ namespace laba_graph_3
                 }
                 // pictureBox1.Image = bmp;
             }
-            else {
-                    int xtemp1 = x1;
-                    int xtemp2 = x2;
-                    int ytemp1 = y1;
-                    int ytemp2 = y2;
-                    if (Math.Abs(x2 - x1) < Math.Abs(y2 - y1))
+            else
+            {
+                if (isfirst)
+                {
+                    bool steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+                    if (steep)
                     {
-                        (x1, y1) = (y1, x1);
-                        (x2,y2) = (y2,x2);
+                        Swap(ref x1, ref y1);
+                        Swap(ref x2, ref y2);
                     }
+
                     if (x1 > x2)
                     {
-                        (x1, x2) = (x2, x1);
-                        (y1, y2) = (y2, y1);
+                        Swap(ref x1, ref x2);
+                        Swap(ref y1, ref y2);
                     }
-                    float dx = x2 - x1;
-                    float dy = y2 - y1;
-                    float gradient = dy / dx;
+
+                    int dx = x2 - x1;
+                    int dy = y2 - y1;
+                    float gradient = (float)dy / dx;
+                    float xgapg = (float)(1 - fpart(x1 + 0.5));
                     float y = y1 + gradient;
-                    for (var x = x1 + 1; x <= x2 - 1; x++)
+
+                    for (int x = x1 + 1; x < x2; x++)
                     {
-                        DrawPoint(Math.Abs(xtemp2 - xtemp1) < Math.Abs(ytemp2 - ytemp1) ? (int)y : x, Math.Abs(xtemp2 - xtemp1) < Math.Abs(ytemp2 - ytemp1) ? x : (int)y, 1 - (y - (int)y));
-                        DrawPoint(Math.Abs(xtemp2 - xtemp1) < Math.Abs(ytemp2 - ytemp1) ? (int)y + 1 : x, Math.Abs(xtemp2 - xtemp1) < Math.Abs(ytemp2 - ytemp1) ? x : (int)y + 1, y - (int)y);
+                        if (steep)
+                        {
+                            DrawPixel(bmp, (int)y, x, GetInterpolation((float)fpart(y)));
+                            DrawPixel(bmp, (int)y + 1, x, GetInterpolation((float)((1 - fpart(y)))));
+                        }
+                        else
+                        {
+                            DrawPixel(bmp, x, (int)y, GetInterpolation((float)(fpart(y))));
+                            DrawPixel(bmp, x, (int)y + 1, GetInterpolation((float)((1 - fpart(y)))));
+                        }
+
                         y += gradient;
-                    }      
+                    }
+                    pictureBox1.Image = bmp;
+                    pictureBox1.Invalidate();
+                }
+            }
+
+        }
+
+
+
+        private void DrawPixel(Bitmap bitmap, int x, int y, float intensity)
+        {
+            if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+            {
+                Color pixelColor = Color.FromArgb((int)(255 * (1 - intensity)), Color.Black);
+                bmp.SetPixel(x, y, pixelColor);
+            }
+            
+        }
+
+        private float GetInterpolation(float value)
+        {
+            if (value <= 0.0)
+            {
+                return 0.0f;
+            }
+            else if (value >= 1.0)
+            {
+                return 1.0f;
+            }
+            else
+            {
+                return value;
             }
         }
 
-
-        void DrawPoint(int x, int y, float intensive)
+        private void Swap(ref int x, ref int y)
         {
-            Color col = Color.Black;
-            bmp.SetPixel(x, y, Color.FromArgb(255, (int)(col.R * (1 - intensive)), (int)(col.G * (1 - intensive)), (int)(col.B * (1 - intensive))));
-            pictureBox1.Image = bmp;
-            pictureBox1.Invalidate();
+            int step = y;
+            y = x;
+            x = step;
         }
 
-        private void CheckMethod() {
+
+
+
+
+        private void CheckMethod()
+        {
             if (radioButton1.Checked)
                 isBresen = true;
             else
                 isBresen = false;
-            
-        }
-
-
-
-        private void pictureBox2_MouseDown(object sender, MouseEventArgs e)
-        {
 
         }
-
-       
-
-
 
     }
 }
