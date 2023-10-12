bc14b957afa9f437a997c89c106321f17811a90e
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-10-12 09:40:17 +0300

    indiv v1

--- jarvis/Form1.Designer.cs
+++ jarvis/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -69,6 +70,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Сгенерировать";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -78,12 +80,24 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Обернуть";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(705, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -104,6 +118,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- jarvis/Form1.cs
+++ jarvis/Form1.cs
@@ -12,9 +12,107 @@ namespace jarvis
 {
     public partial class Form1 : Form
     {
+        List<Point> points;
+        List<Point> boundPoints;
+        Bitmap bmp;
+        Pen Black;
+        SolidBrush BlackBrush;
         public Form1()
         {
             InitializeComponent();
+            points= new List<Point>();
+            boundPoints= new List<Point>();
+            bmp = new Bitmap(pictureBox1.Width,pictureBox1.Height);
+            Black = new Pen(Color.Black);
+            BlackBrush= new SolidBrush(Color.Black);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GenPoints();
+        }
+
+        private void GenPoints()
+        {
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            int pointCount = int.Parse(textBox1.Text);
+            Random r = new Random();
+            for (int i = 0; i < pointCount; i++)
+            {
+                int x = r.Next(100, width - 100);
+                int y = r.Next(100, height - 100);
+                Point p = new Point(x, y);
+
+                while (points.Contains(p))
+                {
+                    x = r.Next(100,width-100);
+                    y = r.Next(100,height-100);
+                    p = new Point(x, y);
+                }
+
+                points.Add(p);
+            }
+
+            
+                points.ForEach(x => { bmp.SetPixel(x.X, x.Y, Color.Black); });
+                pictureBox1.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (points.Count < 3)
+                throw new Exception("need more points");
+            Jarvis();
+        }
+
+        private void Jarvis() { 
+            Point start = points.Where(x=>x.X==points.Min(min=>min.X)).First();
+            Point end = points[0];
+
+           
+
+            do {
+                boundPoints.Add(start);
+                for (int i = 1; i < points.Count; i++)
+                {
+                    if (start == end || Rotation(start, end, points[i])==-1)
+                        end = points[i];
+                }
+                start = end;
+            }
+
+            while (start!=boundPoints.First());
+
+            BlackBrush.Color = Color.Red;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                boundPoints.Add(boundPoints.First());
+                boundPoints.ForEach(x => { g.FillEllipse(BlackBrush,x.X-3,x.Y-3,3,3); bmp.SetPixel(x.X, x.Y, Color.Red); });
+                g.DrawLines(Black, boundPoints.ToArray());
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private int Rotation(Point p1, Point p2, Point p) {
+            int rot = (p2.X - p1.X) * (p.Y - p1.Y) - (p.X - p1.X) * (p2.Y - p1.Y);
+
+            if (rot> 0)
+                return -1; 
+            if (rot < 0)
+                return 1;
+            return 0; 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image= bmp;
+            points.Clear();
+            boundPoints.Clear();
+            pictureBox1.Invalidate();
         }
     }
 }
