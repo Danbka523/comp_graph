f5cb031c76c332975cf6a2c69887465d78d4dfe9
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-11-16 12:20:14 +0300

    added save and load

---
+++ laba7/DataManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.LinkLabel;
+
+namespace laba7
+{
+    internal class DataManager
+    {
+
+        public DataManager() {
+        
+        }
+
+        public void Save(string filename, Polyhedron figure) {
+            var verts = figure.GetVerts();
+            var docPath = Environment.CurrentDirectory;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, filename)))
+            {
+                foreach (var v in verts) {
+                    outputFile.WriteLine($"v {v.X} {v.Y} {v.Z}");
+                }
+                foreach (var p in figure.Polygons)
+                {
+                    outputFile.Write($"f ");
+                    foreach (var line in p.Lines)
+                    {
+                        outputFile.Write($"{verts.IndexOf(line.Start)+1} ");
+
+                    }
+                    outputFile.WriteLine();
+                }
+            }
+        }
+
+
+        public Polyhedron Load(string filePath) {
+            var verts = new List<Point>();
+            var fig_lines = new List<Line>();
+            var lines = File.ReadAllLines(filePath);
+            int skipCount = 0;
+            foreach (var line in lines)
+            {
+                var t = line.Split(" ");
+                if (t[0] == "v") {
+                    Point p = new Point(float.Parse(t[1], CultureInfo.InvariantCulture),
+                        float.Parse(t[2], CultureInfo.InvariantCulture),
+                        float.Parse(t[3], CultureInfo.InvariantCulture));
+                    verts.Add(p);
+                      
+                        
+                }
+                if (t[0] == "f") {
+                    var f = line.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                    skipCount = f.Length - 1;
+                    for (int i = 1; i < f.Length-1; i++)
+                    {
+                        fig_lines.Add(new Line(verts[int.Parse(f[i]) - 1], verts[int.Parse(f[i + 1]) - 1]));
+                    }
+                    fig_lines.Add(new Line(verts[int.Parse(f[1]) - 1], verts[int.Parse(f.Last()) - 1]));
+                }
+
+      
+            }
+
+            
+
+            List<Polygon> polygons = new List<Polygon>();
+            for (int i = 0; i < fig_lines.Count(); i+=skipCount) {
+                polygons.Add(new Polygon(fig_lines.Skip(i).Take(skipCount).ToList()));
+            }
+            return new Polyhedron().AddPolygons(polygons);
+        }
+
+    }
+}
---
+++ laba7/Polyhedron.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba7
+{
+    internal class Polyhedron
+    {
+        List<Polygon> polygons;
+
+        public Polyhedron() { 
+            polygons = new List<Polygon>();
+        }
+
+        public Polyhedron AddPolygon(Polygon poly) {
+            polygons.Add(poly);
+            return this;
+        }
+
+        public Polyhedron AddPolygons(List<Polygon> polys) { 
+            polygons.AddRange(polys);
+            return this;
+        }
+
+        public List<Polygon> Polygons { get { return polygons; } }
+
+        public List<Point> GetVerts() { 
+            List<Point> verts = new();
+            foreach (Polygon poly in polygons)
+            {
+                foreach (Line lines in poly.Lines)
+                {
+                    if (!verts.Contains(lines.Start))
+                        verts.Add(lines.Start);
+                }
+
+            }
+
+            return verts;
+        }
+
+    
+    }
+
+
+}
