2bf7ddb72a7209db641252b7b073a497952f05f9
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-12-16 10:22:24 +0300

    laba13 init

---
+++ laba13/Headers.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <GL/glew.h>
+
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include<SOIL/SOIL.h> 
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
---
+++ laba13/Mesh.h
@@ -0,0 +1,18 @@
+#pragma once
+#include"Headers.h"
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<vector>
+using namespace std;
+
+
+class Mesh {
+
+	vector<float> vertices;
+	GLuint VBO;
+
+	void parse_file(string psth) {
+		
+	}
+};
